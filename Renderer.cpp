57be8b6dfabc25dbb08cd33cec440aeb466a6049
TGoncpp <141250817+TGoncpp@users.noreply.github.com>
2023-09-26 11:03:07 +0200

    Add project files.

---
+++ Renderer.cpp
@@ -0,0 +1,163 @@
+//External includes
+#include "SDL.h"
+#include "SDL_surface.h"
+
+//Project includes
+#include "Renderer.h"
+#include "Math.h"
+#include "Matrix.h"
+#include "Material.h"
+#include "Scene.h"
+#include "Utils.h"
+
+using namespace dae;
+
+Renderer::Renderer(SDL_Window * pWindow) :
+	m_pWindow(pWindow),
+	m_pBuffer(SDL_GetWindowSurface(pWindow))
+{
+	//Initialize
+	SDL_GetWindowSize(pWindow, &m_Width, &m_Height);
+	m_pBufferPixels = static_cast<uint32_t*>(m_pBuffer->pixels);
+}
+
+void Renderer::Render(Scene* pScene) const
+{
+	Camera& camera = pScene->GetCamera();
+	auto& materials = pScene->GetMaterials();
+	auto& lights = pScene->GetLights();
+
+
+	//Vector3 direction{ 0,0,1 };
+	//Ray ray{ {0,0,0}, direction };
+	//HitRecord hitRecord{};
+	//float ar = static_cast<float>(m_Width) / static_cast<float>(m_Height);
+
+	//Sphere sphere;
+	////sphere.origin = { m_Width / 2.0f, m_Height / 2.0f, 0.0f };
+	//sphere.origin = { 0.0f, 0.0f, 100.0f };
+	//sphere.radius = 50.0f;
+	//ColorRGB finalColor{  };
+
+	//for (int px{}; px < m_Width; ++px)
+	//{
+
+	//	for (int py{}; py < m_Height; ++py)
+	//	{
+
+	//		float pxc{ (float)px + 0.5f };
+	//		direction.x = ( (2 * pxc) / static_cast<float>(m_Width) - 1 ) * ar;
+
+	//		direction.y = 1 - ((2 * (static_cast<float>(py) + 0.5f)) / static_cast<float>(m_Height) );
+
+	//		direction.Normalize();
+	//		ray.direction = direction;
+
+	//		if (GeometryUtils::HitTest_Sphere(sphere, ray, hitRecord))
+	//		{
+	//			const float scale_t{ (hitRecord.t - 50.0f) / 40.0f };
+	//			ColorRGB Color{ scale_t,scale_t, scale_t };
+	//			finalColor = Color;
+	//		}
+
+	//		else
+	//		{
+	//			ColorRGB Color{ 0,0, 0 };
+	//			finalColor = Color;
+	//		}
+
+
+	//		//Update Color in Buffer
+	//		finalColor.MaxToOne();
+
+	//		m_pBufferPixels[px + (py * m_Width)] = SDL_MapRGB(m_pBuffer->format,
+	//			static_cast<uint8_t>(finalColor.r * 255),
+	//			static_cast<uint8_t>(finalColor.g * 255),
+	//			static_cast<uint8_t>(finalColor.b * 255));
+	//	}
+	//}
+
+	////@END
+	////Update SDL Surface
+	//SDL_UpdateWindowSurface(m_pWindow);
+
+
+
+	float x{}, y{};
+	float ar = float(m_Width) / float(m_Height);
+	Ray ray{  };
+	HitRecord hitRecord{};
+	ColorRGB finalColor{  };
+
+
+	for (int px{}; px < m_Width; ++px)
+	{
+			float pxc = float(px) + .5f;
+			x = (2 * pxc / float(m_Width)-1) * ar;
+
+		for (int py{}; py < m_Height; ++py)
+		{
+			// create ray direction vector
+			float pyc = float(py) + .5f;
+			y = 1 - 2 * pyc / float(m_Height);
+			ray.direction.x = x;
+			ray.direction.y = y;
+			ray.direction.z = 1.0f;
+			ray.direction.Normalize();
+
+			//raytrace scene
+			pScene->GetClosestHit(ray, hitRecord);
+
+			//render
+			if (hitRecord.didHit)
+			{
+				finalColor = materials[hitRecord.materialIndex]->Shade();
+				hitRecord.didHit = false;
+			}
+			else
+			{
+				finalColor = {};
+			}
+
+			/////////////////////////////////////////////////////////////////////////////////////////
+			////check for correct raydirection 
+			//ColorRGB finalColor{ ray.direction.x ,ray.direction.y, ray.direction.z };
+
+			////////////////////////////////////////////////////////////////////////////////////////
+			
+			////check plane test
+			//Plane testPlane{ {0.0f,-50.0f, 0.0f }, {.0f, 1.0f, .0f}, 0 };
+			//if (GeometryUtils::HitTest_Plane(testPlane, ray, hitRecord))
+			//{
+			//	const float test_t{ hitRecord.t / 500.f };
+			//	//finalColor = materials[hitRecord.materialIndex]->Shade();
+			//	ColorRGB color{ test_t, test_t, test_t };
+			//	finalColor = color;
+			//}
+			//else
+			//{
+			//	finalColor = {};
+			//}
+
+			//////////////////////////////////////////////////////////////////////////////////////////
+			//Update Color in Buffer
+			finalColor.MaxToOne();
+
+			m_pBufferPixels[px + (py * m_Width)] = SDL_MapRGB(m_pBuffer->format,
+				static_cast<uint8_t>(finalColor.r * 255),
+				static_cast<uint8_t>(finalColor.g * 255),
+				static_cast<uint8_t>(finalColor.b * 255));
+		}
+	}
+
+	//@END
+	//Update SDL Surface
+	SDL_UpdateWindowSurface(m_pWindow);
+
+}
+
+
+bool Renderer::SaveBufferToImage() const
+{
+	return SDL_SaveBMP(m_pBuffer, "RayTracing_Buffer.bmp");
+}
---
+++ Utils.h
@@ -0,0 +1,196 @@
+#pragma once
+#include <cassert>
+#include <fstream>
+#include "Math.h"
+#include "DataTypes.h"
+
+namespace dae
+{
+	namespace GeometryUtils
+	{
+#pragma region Sphere HitTest
+		//SPHERE HIT-TESTS
+		inline bool HitTest_Sphere(const Sphere& sphere, const Ray& ray, HitRecord& hitRecord, bool ignoreHitRecord = false)
+		{
+			//todo W1			
+			const float A{ Vector3::Dot(ray.direction, ray.direction) };
+			const float B{ 2.0f * Vector3::Dot(ray.origin - sphere.origin, ray.direction) };
+			const float C{ Vector3::Dot(ray.origin - sphere.origin, ray.origin - sphere.origin) - sphere.radius * sphere.radius };
+
+			const float Discrimenant{ (B * B - 4.0f * A * C) };
+			if (Discrimenant < 0)
+				return false;
+
+			const float t{ (-B - sqrtf(Discrimenant)) / (2.0f * A) };
+
+			if (t >= ray.max || 
+				t < ray.min  || 
+				ignoreHitRecord )
+				return false;
+
+			hitRecord.t = t;
+			hitRecord.didHit = true;
+			hitRecord.origin = ray.origin + ray.direction * t ;
+			hitRecord.materialIndex = sphere.materialIndex;
+			hitRecord.normal = (hitRecord.origin - sphere.origin) / sphere.radius;
+
+			return true;	
+				
+		}
+
+		inline bool HitTest_Sphere(const Sphere& sphere, const Ray& ray)
+		{
+			HitRecord temp{};
+			return HitTest_Sphere(sphere, ray, temp, true);
+		}
+#pragma endregion
+#pragma region Plane HitTest
+		//PLANE HIT-TESTS
+		inline bool HitTest_Plane(const Plane& plane, const Ray& ray, HitRecord& hitRecord, bool ignoreHitRecord = false)
+		{
+			//todo W1
+			hitRecord.t = Vector3::Dot((plane.origin - ray.origin), plane.normal) / Vector3::Dot(ray.direction, plane.normal);
+			if (hitRecord.t < ray.max && 
+				hitRecord.t > ray.min &&
+				!ignoreHitRecord)
+			{
+				hitRecord.origin = ray.origin + ray.direction * hitRecord.t ;
+				hitRecord.didHit = true;
+				hitRecord.materialIndex = plane.materialIndex;
+				//hitRecord.normal = ;
+				return true;
+			}
+
+			return false;
+		}
+
+		inline bool HitTest_Plane(const Plane& plane, const Ray& ray)
+		{
+			HitRecord temp{};
+			return HitTest_Plane(plane, ray, temp, true);
+		}
+#pragma endregion
+#pragma region Triangle HitTest
+		//TRIANGLE HIT-TESTS
+		inline bool HitTest_Triangle(const Triangle& triangle, const Ray& ray, HitRecord& hitRecord, bool ignoreHitRecord = false)
+		{
+			//todo W5
+			assert(false && "No Implemented Yet!");
+			return false;
+		}
+
+		inline bool HitTest_Triangle(const Triangle& triangle, const Ray& ray)
+		{
+			HitRecord temp{};
+			return HitTest_Triangle(triangle, ray, temp, true);
+		}
+#pragma endregion
+#pragma region TriangeMesh HitTest
+		inline bool HitTest_TriangleMesh(const TriangleMesh& mesh, const Ray& ray, HitRecord& hitRecord, bool ignoreHitRecord = false)
+		{
+			//todo W5
+			assert(false && "No Implemented Yet!");
+			return false;
+		}
+
+		inline bool HitTest_TriangleMesh(const TriangleMesh& mesh, const Ray& ray)
+		{
+			HitRecord temp{};
+			return HitTest_TriangleMesh(mesh, ray, temp, true);
+		}
+#pragma endregion
+	}
+
+	namespace LightUtils
+	{
+		//Direction from target to light
+		inline Vector3 GetDirectionToLight(const Light& light, const Vector3 origin)
+		{
+			//todo W3
+			assert(false && "No Implemented Yet!");
+			return {};
+		}
+
+		inline ColorRGB GetRadiance(const Light& light, const Vector3& target)
+		{
+			//todo W3
+			assert(false && "No Implemented Yet!");
+			return {};
+		}
+	}
+
+	namespace Utils
+	{
+		//Just parses vertices and indices
+#pragma warning(push)
+#pragma warning(disable : 4505) //Warning unreferenced local function
+		static bool ParseOBJ(const std::string& filename, std::vector<Vector3>& positions, std::vector<Vector3>& normals, std::vector<int>& indices)
+		{
+			std::ifstream file(filename);
+			if (!file)
+				return false;
+
+			std::string sCommand;
+			// start a while iteration ending when the end of file is reached (ios::eof)
+			while (!file.eof())
+			{
+				//read the first word of the string, use the >> operator (istream::operator>>) 
+				file >> sCommand;
+				//use conditional statements to process the different commands	
+				if (sCommand == "#")
+				{
+					// Ignore Comment
+				}
+				else if (sCommand == "v")
+				{
+					//Vertex
+					float x, y, z;
+					file >> x >> y >> z;
+					positions.push_back({ x, y, z });
+				}
+				else if (sCommand == "f")
+				{
+					float i0, i1, i2;
+					file >> i0 >> i1 >> i2;
+
+					indices.push_back((int)i0 - 1);
+					indices.push_back((int)i1 - 1);
+					indices.push_back((int)i2 - 1);
+				}
+				//read till end of line and ignore all remaining chars
+				file.ignore(1000, '\n');
+
+				if (file.eof()) 
+					break;
+			}
+
+			//Precompute normals
+			for (uint64_t index = 0; index < indices.size(); index += 3)
+			{
+				uint32_t i0 = indices[index];
+				uint32_t i1 = indices[index + 1];
+				uint32_t i2 = indices[index + 2];
+
+				Vector3 edgeV0V1 = positions[i1] - positions[i0];
+				Vector3 edgeV0V2 = positions[i2] - positions[i0];
+				Vector3 normal = Vector3::Cross(edgeV0V1, edgeV0V2);
+
+				if(isnan(normal.x))
+				{
+					int k = 0;
+				}
+
+				normal.Normalize();
+				if (isnan(normal.x))
+				{
+					int k = 0;
+				}
+
+				normals.push_back(normal);
+			}
+
+			return true;
+		}
+#pragma warning(pop)
+	}
+}
